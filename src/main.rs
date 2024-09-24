eb41bbdac124f59c2615e0f2b6d9eb11daa3f23a
John Waczak <john.louis.waczak@gmail.com>
2024-09-24 10:47:57 -0500

    udpated to plot a basic sphere

--- src/main.rs
+++ src/main.rs
@@ -30,12 +30,25 @@ struct Args {
 
 
 
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = r.direction().dot(r.direction());
+    let b = 2.0 * oc.dot(r.direction());
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
+}
+
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = r.direction().normalized();
     let s = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - s) * Color::new(1.0, 1.0, 1.0) + s * Color::new(0.5, 0.7, 1.0)
 }
-
 
 
 fn main() {
@@ -57,13 +70,15 @@ fn main() {
     let lower_left_corner = origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length);
 
 
+
+    
     // use par_enumerate_pixels_mut for parallel execution
     img.enumerate_pixels_mut()
         .for_each(|(i, jj, pixel)|{
             // NOTE: PNG origin is (0,0) -> Top Left
             //       flip it so the img coords are
             //       aligned with the camera
-            let j = 255 - jj;
+            let j = args.height - jj;
 
             let u = (i as f64) / ((args.width - 1) as f64);
             let v = (j as f64) / ((args.height- 1) as f64);
